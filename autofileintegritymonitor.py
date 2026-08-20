9be47397d155edae84c73ac09d651edcbd7d7a17
Ahad <ahaddilanov@gmail.com>
2026-08-21 00:37:30 +0400

    Add baseline saving to JSON

--- autofileintegritymonitor.py
+++ autofileintegritymonitor.py
@@ -1,6 +1,6 @@
 import hashlib
 import os
-
+import json
 
 def hash_file(filepath):
     with open(filepath, "rb") as f:
@@ -17,5 +17,10 @@ def hash_folder(folder_path):
     return file_hashes
 
 
-folder_result = hash_folder(".")
-print(folder_result)
+def save_baseline(file_hashes, output_file= "baseline.json"):
+    with open(output_file, "w") as f:
+        json.dump(file_hashes, f, indent=4)
+
+folder_result= hash_folder(".")
+save_baseline(folder_result)
+print("Baseline saved!")
